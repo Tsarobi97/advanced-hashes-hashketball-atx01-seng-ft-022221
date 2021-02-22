dda075e2fd5811ecd38d8ba179cbb5c3565cb5f1
Robert Obi <robobi97@googlemail.com>
2021-02-22 20:26:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ player_num = []
 if team_name == game_hash[:home][:team_name]
  game_hash[:home][:players].each do |player|
    player_num.push(player[:number])
-   elsif 
+ else
    game_hash[:away][:players].each do |player|
    player_num.push(player[:number])
  end 
